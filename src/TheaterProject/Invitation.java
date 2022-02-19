d810abdf41d4aa21cdf7d9162e1b21ac4030d335
Suntory(SY KIM) <54302155+seyoung755@users.noreply.github.com>
2022-02-19 15:36:06 +0900

    [소극장 예제 1] 초기 코드 구현 (#1)
    * feat: 초대장 객체 구현
    
    - 이벤트에 당첨된 관람객이 가지고 있는 초대장 객체 구현하였습니다.
    
    * feat: 관람권 객체 생성
    
    - 손님이 공연을 보기 위해 필요한 티켓 객체 생성
    
    * feat: 관객 객체 생성
    
    - 공연을 보는 관객 객체를 생성하고 관객은 각자의 가방을 지니고 있다고 하였습니다.
    
    * feat: 가방 객체 구현
    
    - 가방에는 초대권, 현금, 티켓이 들어있다.
    - 모든 관객은 가방을 가지고 있고, 초대권과 현금을 가지고 있거나, 현금만 가지고 있는 경우만 있다.
    - 초대권이나 티켓이 있는지 확인하는 메서드를 가진다.
    - 티켓을 주입받는 setter를 가진다.
    - 가진 현금을 조정하는 메서드들을 가진다.
    
    * feat: 티켓을 교환하는 티켓 창구 객체 구현
    
    - 매표소에서는 ticket을 List형태로 보관하고 있다.
    - 손님이 티켓을 사가면 amount를 반영하고, ticket 리스트에서 맨 첫 요소를 반환한다.
    
    * feat: 티켓 판매원 객체 생성
    
    - 티켓 판매원은 자기가 일하는 매표소를 주입받는다.
    - 해당 매표소의 로직에 따라 판매를 담당할 것이다.
    
    * feat: 극장 객체 구현
    
    - 관객이 공연을 보는 장소인 극장 객체를 구현하였습니다.
    - 극장에서 입장을 받으면서 초대권 유무에 따라 티켓으로 교환하거나 판매하는 일을 합니다.
    - 하지만 문제점으로 모든 역할을 Theater에서 담당하고 있습니다.
    - 내부 로직이 모두 enter에 구현되어 있기 때문입니다.

---
+++ src/TheaterProject/Invitation.java
@@ -0,0 +1,11 @@
+package TheaterProject;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+
+    public Invitation(LocalDateTime when) {
+        this.when = when;
+    }
+}
---
+++ src/TheaterProject/Ticket.java
@@ -0,0 +1,13 @@
+package TheaterProject;
+
+public class Ticket {
+    private Long fee;
+
+    public Ticket(Long fee) {
+        this.fee = fee;
+    }
+
+    public Long getFee() {
+        return fee;
+    }
+}
